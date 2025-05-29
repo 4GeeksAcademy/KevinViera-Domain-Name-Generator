008cfe272048008479519fbbbf858dc343e72b91
KevinViera <kyohanviera@gmail.com>
2025-05-29 00:37:52 +0000

    this is my first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the','our','a']
+  let adj = ['huge','smart','strong']
+  let noun = ['giant','guitar','dragon']
+  let ext = ['.com','.net','.dev']
+
+  for (let elm of pronoun) {
+    for (let elm2 of adj) {
+      for (let elm3 of noun) {
+        for (let elm4 of ext) {
+          let result = elm + elm2 + elm3 + elm4;
+            console.log(result);
+              document.querySelector('#name').innerHTML += "<ul>" + result + "</ul>"
+        }
+      }
+    }
+  }
+}
